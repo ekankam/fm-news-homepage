688dbb1c973d7a8227a55e6c17ba53a962220d40
ekankam <eb.kankam26@gmail.com>
2023-01-16 17:58:08 -0600

    More tailwind configurations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     colors: {
-      "off-white": "Off-white: hsl(36, 100%, 99%)",
+      "off-white": "hsl(36, 100%, 99%)",
       "gun-memtal": "hsl(236, 13%, 42%)",
       "dark-space-blue": "hsl(240, 100%, 5%)",
       "silver": "hsl(233, 8%, 79%)",
