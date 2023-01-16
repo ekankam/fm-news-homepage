f9f9f5a8e58c0bede1edfd28a5fed93ad2bc3248
ekankam <eb.kankam26@gmail.com>
2023-01-16 17:37:26 -0600

    initializing application

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      "off-white": "Off-white: hsl(36, 100%, 99%)",
+      "gun-memtal": "hsl(236, 13%, 42%)",
+      "dark-space-blue": "hsl(240, 100%, 5%)",
+      "silver": "hsl(233, 8%, 79%)",
+      "soft-red": "hsl(5, 85%, 63%)",
+      "soft-orange": "hsl(35, 77%, 62%)"
+    },
+    fontSize: {
+      "56": "56px"
+    }
+  },
+  plugins: [],
+}
