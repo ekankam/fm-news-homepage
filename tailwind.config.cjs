36f14d9ee79609476c0017da9fab99d703b55cb2
ekankam <eb.kankam26@gmail.com>
2023-01-17 20:52:03 -0600

    Tweak tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,17 +5,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      "off-white": "hsl(36, 100%, 99%)",
-      "gun-memtal": "hsl(236, 13%, 42%)",
-      "dark-space-blue": "hsl(240, 100%, 5%)",
-      "silver": "hsl(233, 8%, 79%)",
-      "soft-red": "hsl(5, 85%, 63%)",
-      "soft-orange": "hsl(35, 77%, 62%)",
-      "overlay": "#00001A94"
-    },
-    fontSize: {
-      "56": "56px"
+    extend: {
+      colors: {
+        "off-white": "hsl(36, 100%, 99%)",
+        "gun-memtal": "hsl(236, 13%, 42%)",
+        "dark-space-blue": "hsl(240, 100%, 5%)",
+        "silver": "hsl(233, 8%, 79%)",
+        "soft-red": "hsl(5, 85%, 63%)",
+        "soft-orange": "hsl(35, 77%, 62%)",
+        "overlay": "#00001A94"
+      },
+      fontSize: {
+        "56": "56px"
+      }
     }
   },
   plugins: [],
